e7039ce3e432a3eda20eed982fe97a15d0e31670
yotammanor <yotammanor@gmail.com>
2013-12-14 21:14:17 +0200

    First version of game is working

--- facebook_aggregator_pilot/facebook_try.py
+++ facebook_aggregator_pilot/facebook_try.py
@@ -1,8 +1,10 @@
+from random import randrange
+
 __author__ = 'yotam'
 
 import facebook
 from pprint import pprint
-token = 'CAACEdEose0cBAAq1IEnXnZBPb72nMZB7FeySIst8oZCKR7OMiNZCpuzPLYVurQEsNVbZAXtIMx6HyDgW4YSE2ESJsGlKZCdXwaHFIU6WkIMopX7DUYvCe2cQtbfZBUfj61isPleDkrgr74HMDmuXZBfQWV1fXtvyywMmfSbcHIYGN1nhUgfndyYHjovtsQ4WStMZD'
+token = 'CAACEdEose0cBAEZAoiPZAaRLCu2NIPTyVEK9ZCQNbWSnkNoNUhmA0uuIbQZAD9v1lltnkjQ8EKp9uMJHunLUG5khhAkLOX8Pb1lKArHT5a1j9TDnIxlAczh2QZBhzw8rIIgZBbnqRgewWUbQdnFC7ML4z7x3e3bWoLxE3uDNsHb5lIwZCeyEnGC7SvqmFMjnrR7K5r4eBGRVQZDZD'
 graph = facebook.GraphAPI(access_token=token)
 
 profile = graph.get_object("me")
@@ -12,6 +14,7 @@ list_of_friends = [friend['name'] for friend in friends['data']]
 
 pprint(len(list_of_friends))
 
+
 def get_last_status_message(graph_object, page_id):
 
     all_posts = graph_object.get_connections(id=page_id, connection_name='posts')
@@ -20,3 +23,11 @@ def get_last_status_message(graph_object, page_id):
     status_message = list_of_posts[0]  # First Message on the list - with the latest date
     return status_message
 
+
+def get_random_status_message(graph_object, page_id):
+
+    all_posts = graph_object.get_connections(id=page_id, connection_name='posts')
+    list_of_posts = all_posts['data']
+    list_of_posts.sort(key=lambda x: x['created_time'], reverse=True)
+    status_message = list_of_posts[randrange(len(list_of_posts))]  # A random post from the list
+    return status_message
--- facebook_aggregator_pilot/views.py
+++ facebook_aggregator_pilot/views.py
@@ -5,25 +5,46 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
 import facebook
+from random import randrange
+
+
+PAGES_OF_POLITICIANS = ['naftalibennett', 'YairLapid', 'ShellyYachimovich', 'barackobama']
 
 
 def get_last_status_message(graph_object, page_id):
 
-    all_posts = graph_object.get_connections(id=page_id, connection_name='posts')
+    all_posts = graph_object.get_connections(id=page_id, connection_name='posts', args={"limit": "20"})
     list_of_posts = all_posts['data']
     list_of_posts.sort(key=lambda x: x['created_time'], reverse=True)
     status_message = list_of_posts[0]  # First Message on the list - with the latest date
     return status_message
 
 
+def get_random_status_message(graph_object, page_id):
+
+    all_posts = graph_object.get_connections(id=page_id, connection_name='posts', args={"limit": "20"})
+    list_of_posts = all_posts['data']
+    list_of_posts.sort(key=lambda x: x['created_time'], reverse=True)
+    status_message = list_of_posts[randrange(len(list_of_posts))]  # A random post from the list
+    return status_message
+
+
+def get_list_of_pages(number_of_pages):
+    local_pages_of_politicians = PAGES_OF_POLITICIANS
+    selected_list_of_pages = list()
+    while len(selected_list_of_pages)<=number_of_pages:
+        selected_list_of_pages.append(local_pages_of_politicians.pop(randrange(len(local_pages_of_politicians))))
+    return selected_list_of_pages
+
+
 # Create your views here.
 def index(request):
-    token = 'CAACEdEose0cBAHrmTnLQq3Wc5OPPEbeapFRUcSfNp1nHwKSbawDQaHZByq9UHiFyB3Uv1RNfxsQxCEKu0pRC76nKutDKyxER3Dx4P97pYEBZAZCLKOH8ZCEs8MU2IOkWwgWNUxB3MzB48NCYFf7GbtWvYKThUDBbuQGAGSXIUbpN6iLFD2z2ZAMDDC17WklYRvVV3sBI0RwZDZD'
+    token = 'CAACEdEose0cBAJ1slET8JLburo1Xli3wwXuynK5ktBqn1WWWNUkCyjx9q7FVoO9ZCCHddpEAJi9cKewGoZCSiH7H4VwH941pZAiQlOxBaUjAPpZA4MWb3TsQcrYyBjwZA40hi2j0ISJGFvdFDIyTV2TuJ7nIo85D0KIWb4ASk9rlZBjNA9Bb2MErVUqEUA3Q4ZD'
 
     graph = facebook.GraphAPI(access_token=token)
-    list_of_pages = ['naftalibennett', 'YairLapid', 'ShellyYachimovich']
+    list_of_pages = get_list_of_pages(number_of_pages=3)
 
-    list_of_messages = [get_last_status_message(graph, page_id)['message'] for page_id in list_of_pages]
+    list_of_messages = [get_random_status_message(graph, page_id) for page_id in list_of_pages]
 
     context = {'list_of_messages': list_of_messages}
     return render(request, 'index.html', context)
